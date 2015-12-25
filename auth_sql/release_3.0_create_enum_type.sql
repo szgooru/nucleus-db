ca22a426c7dd06a3779de62e1f168992ecef3146
Sheeban Singaram <sheeban.s@goorulearning.org>
2015-12-25 15:01:16 +0530

    updated the Sql patch

--- auth_sql/release_3.0_create_enum_type.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE user_identity_type AS ENUM ('google', 'wsfed','smal', 'credential');
-CREATE TYPE user_identity_status AS ENUM ('google', 'wsfed','smal', 'credential');
-CREATE TYPE user_gender AS ENUM ('male', 'female','other', 'not_wise_to_share');
-CREATE TYPE user_category AS ENUM ('Teacher', 'Student', 'Parent', 'Other');
