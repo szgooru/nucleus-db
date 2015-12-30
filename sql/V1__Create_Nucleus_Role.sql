2eb1026bbab67f248e0e64ae1408e3d79c68791c
aagooru <amit@gooru.org>
2015-12-30 16:05:59 +0530

    - Remove seed files from sql directory (moved to archive)

--- sql/V1__Create_Nucleus_Role.sql
+++
@@ -1,6 +0,0 @@
--- You can run this command using the default postgres (aka superuser) role created by postgres:
--- psql -U postgres -f <path to>/V1__Create_Nucleus_Role.sql
-
--- DROP ROLE nucleus;
-CREATE ROLE nucleus WITH INHERIT LOGIN REPLICATION PASSWORD 'nucleus';
-
